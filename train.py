cf870ed09531070915e2440d18bd273fcb9a729a
Denys <denya.boyko@gmail.com>
2021-07-18 17:31:58 +0200

    Model Saving during training done

--- train.py
+++ train.py
@@ -15,6 +15,15 @@ device = torch.device("cuda")
 epochs = 10
 lr = 0.001
 seq_lenght = 88200
+model_save_path = 'models\\'
+model_name = '1rnn221v1.tar' # 1 - number of layers of rnn; rnn - type of layer; 221 -> (input size, hidden_size, number of layers; v - version)
+
+
+
+def save_mod(state, path, name):
+
+	torch.save(state, path+name)
+
 
 def load_Data(data_path):
     waveform, sample_rate = torchaudio.load( data_path ) #supports only wav files P.S.: Waveform can be to chanaled --> 2 dim tensor
@@ -22,16 +31,21 @@ def load_Data(data_path):
     #print(sample_rate)
     return waveform, sample_rate
 
+
 def iterator(waveform):
     pass
+
 
 def convert_data(data):
     data = data.unsqueeze(0)
     data = data.permute(2, 0, 1)
     return data
 
+
 def unconvert_data():
     pass
+
+
 #waveform = waveform[:][:10000].unsqueeze(0)
 
 
@@ -57,25 +71,34 @@ loss_function = torch.nn.MSELoss()
 
 
 #for track in os.listdir("data\\"):
-for epoch in range(epochs):
+try:
+    checkpoint = torch.load(model_save_path + model_name)
+    model.load_state_dict(checkpoint["state_dictionary"])
 
-    for song in range(1, 5):
-        data_path = os.path.join("data", f"sample ({song}).wav")
-        waveform, sample_rate = load_Data(data_path)
-        song_lenght = waveform.shape[1] - seq_lenght
+except:
 
-        for idx in range(song_lenght):
-            data = convert_data(waveform[:, idx:idx+seq_lenght]).to(device) # takes the part of a song as a sequence
-            label = convert_data(waveform[:, idx+1:idx+1+seq_lenght]).to(device) # takes the part of a song as a sequence the same lenght as data but 1 sample further
+    for epoch in range(epochs):
 
-            output = model(data)
-            loss = loss_function(output[0], label)
-            #print(output[0].shape)
-            #print(output.shape)
-            #plt.plot(output)
+        for song in range(1, 5):
+            data_path = os.path.join("data", f"sample ({song}).wav")
+            waveform, sample_rate = load_Data(data_path)
+            song_lenght = waveform.shape[1] - seq_lenght
+            checkpoint = {"state_dictionary" : model.state_dict()}
+            save_mod(checkpoint, model_save_path, model_name)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            a = input()
+
+            for idx in range(song_lenght):
+                data = convert_data(waveform[:, idx:idx+seq_lenght]).to(device) # takes the part of a song as a sequence
+                label = convert_data(waveform[:, idx+1:idx+1+seq_lenght]).to(device) # takes the part of a song as a sequence the same lenght as data but 1 sample further
+
+                output = model(data)
+                loss = loss_function(output[0], label)
+                #print(output[0].shape)
+                #print(output.shape)
+                #plt.plot(output)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                a = input()
 
